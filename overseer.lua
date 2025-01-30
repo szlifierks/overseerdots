5ea2d13fa23c90a3458fb96513f5df9f2a1ad8cd
szlifierka <185506859+szlifierks@users.noreply.github.com>
2025-01-30 14:59:10 +0100

    Update overseer.lua

--- overseer.lua
+++ overseer.lua
@@ -1,219 +1,128 @@
 return {
   "stevearc/overseer.nvim",
-  config = function()
+  opts = function(_, opts)
     local overseer = require("overseer")
 
-    overseer.setup({
+    opts = vim.tbl_deep_extend("force", opts or {}, {
       task_list = {
         default_detail = 1,
         direction = "bottom",
-        min_height = 10,
+        min_height = 12,
+      },
+      templates = {
+        {
+          name = "Generate CMakeLists",
+          desc = "Auto-generate CMake for C++ projects",
+          builder = function()
+            return {
+              cmd = "sh",
+              args = {
+                "-c",
+                [[
+                cat << EOF > CMakeLists.txt
+cmake_minimum_required(VERSION 3.10)
+project(${PROJECT_NAME:-MyProject})
+set(CMAKE_CXX_STANDARD 20)
+file(GLOB_RECURSE SRC_FILES *.cpp *.hpp)
+add_executable(${PROJECT_NAME:-main} ${SRC_FILES})
+EOF
+                ]],
+              },
+              condition = {
+                callback = function()
+                  local dir = vim.fn.expand("%:p:h")
+                  return vim.fn.filereadable(dir .. "/CMakeLists.txt") == 0
+                    and #vim.fn.glob(dir .. "/*.cpp", false, true) > 0
+                end,
+                filetype = { "cpp", "c" },
+              },
+            }
+          end,
+        },
+        {
+          name = "CMake Configure",
+          desc = "Configure CMake project",
+          builder = function()
+            return {
+              cmd = "cmake",
+              args = { "-B", "build", "-DCMAKE_BUILD_TYPE=Release" },
+              components = {
+                "default",
+                "on_output_quickfix",
+                { "on_complete_notify", timeout = 1 },
+              },
+              condition = {
+                filetype = { "cpp", "c" },
+                callback = function()
+                  return vim.fn.filereadable("CMakeLists.txt") == 1
+                end,
+              },
+            }
+          end,
+        },
+        {
+          name = "CMake Build + Run",
+          desc = "Build and run C++ project",
+          builder = function()
+            return {
+              cmd = "sh",
+              args = {
+                "-c",
+                "cmake --build build --parallel 4 && ./build/main",
+              },
+              components = {
+                "default",
+                "on_output_quickfix",
+                { "on_complete_notify", timeout = 3 },
+              },
+              condition = {
+                filetype = { "cpp", "c" },
+                callback = function()
+                  return vim.fn.filereadable("build/Makefile") == 1 or vim.fn.filereadable("build/build.ninja") == 1
+                end,
+              },
+            }
+          end,
+        },
       },
     })
 
-    local tasks = {
-      {
-        name = "Generate CMakeLists",
-        builder = function()
-          return {
-            cmd = "sh",
-            args = {
-              "-c",
-              [[cat << EOF > CMakeLists.txt
-cmake_minimum_required(VERSION 3.10)
-project(MyProject)
-set(CMAKE_CXX_STANDARD 20)
-file(GLOB SRC_FILES *.cpp)
-add_executable(main ${SRC_FILES})
-EOF
-              ]],
-            },
-            condition = {
-              callback = function()
-                return vim.fn.filereadable("CMakeLists.txt") == 0 and vim.fn.glob("*.cpp") ~= ""
-              end,
-            },
-          }
-        end,
-      },
-
-      {
-        name = "CMake Configure",
-        builder = function()
-          return {
-            cmd = "cmake",
-            args = { "-B", "build", "-DCMAKE_BUILD_TYPE=Release" },
-            components = { "default", "on_output_quickfix" },
-            condition = {
-              filetype = { "cpp", "c" },
-            },
-          }
-        end,
-      },
-
-      {
-        name = "CMake Build + Run",
-        builder = function()
-          return {
-            cmd = "sh",
-            args = {
-              "-c",
-              "cmake --build build --parallel 4 && ./build/main",
-            },
-            components = { "default", "on_output_quickfix" },
-            condition = {
-              filetype = { "cpp", "c" },
-            },
-          }
-        end,
-      },
-
-      {
-        name = "Debug C++",
-        builder = function()
-          return {
-            cmd = "gdb",
-            args = { "-ex", "run", "build/main" },
-            components = { "default" },
-            condition = {
-              filetype = { "cpp", "c" },
-            },
-          }
-        end,
-      },
-
-      -- C# - Build + Run
-      {
-        name = "Build & Run C#",
-        builder = function()
-          return {
-            cmd = "dotnet",
-            args = { "build", "&&", "dotnet", "run" },
-            components = { "default" },
-            condition = {
-              filetype = { "cs" },
-            },
-          }
-        end,
-      },
-
-      {
-        name = "Debug C#",
-        builder = function()
-          return {
-            cmd = "dotnet",
-            args = { "build", "&&", "dotnet", "run", "--configuration", "Debug" },
-            components = { "default" },
-            condition = {
-              filetype = { "cs" },
-            },
-          }
-        end,
-      },
-
-      -- PHP
-      {
-        name = "Run PHP",
-        builder = function()
-          return {
-            cmd = "php",
-            args = { vim.fn.expand("%") },
-            components = { "default" },
-            condition = {
-              filetype = { "php" },
-            },
-          }
-        end,
-      },
-
-      -- JavaScript
-      {
-        name = "Run JavaScript",
-        builder = function()
-          return {
-            cmd = "node",
-            args = { vim.fn.expand("%") },
-            components = { "default" },
-            condition = {
-              filetype = { "javascript" },
-            },
-          }
-        end,
-      },
-
-      -- HTML
-      {
-        name = "Serve HTML",
-        builder = function()
-          return {
-            cmd = "live-server",
-            args = { vim.fn.expand("%:p:h") },
-            components = { "default" },
-            condition = {
-              filetype = { "html" },
-            },
-          }
-        end,
-      },
-
-      {
-        name = "Run Python",
-        builder = function()
-          return {
-            cmd = "python",
-            args = { vim.fn.expand("%") },
-            components = { "default" },
-            condition = {
-              filetype = { "python" },
-            },
-          }
-        end,
-      },
-
-      {
-        name = "Debug Python",
-        builder = function()
-          return {
-            cmd = "python",
-            args = { "-m", "pdb", vim.fn.expand("%") },
-            components = { "default" },
-            condition = {
-              filetype = { "python" },
-            },
-          }
-        end,
-      },
-    }
-
-    for _, task in ipairs(tasks) do
-      overseer.register_template(task)
-    end
-
-    vim.keymap.set("n", "<leader>cc", "<cmd>OverseerRun<CR>", { desc = "Run task" })
-    vim.keymap.set("n", "<leader>ct", "<cmd>OverseerToggle<CR>", { desc = "Toggle tasks" })
-    vim.keymap.set("n", "<leader>cb", "<cmd>OverseerBuild<CR>", { desc = "Build task" })
-    vim.keymap.set("n", "<leader>cd", "<cmd>OverseerDebug<CR>", { desc = "Debug task" })
+    overseer.register_template({
+      name = "Debug C++",
+      desc = "Debug with gdb",
+      builder = function()
+        return {
+          cmd = "gdb",
+          args = { "-ex", "run", "build/main" },
+          components = { "default" },
+          condition = {
+            filetype = { "cpp", "c" },
+          },
+        }
+      end,
+    })
 
     vim.api.nvim_create_autocmd("BufEnter", {
-      pattern = "*",
+      pattern = "*.cpp,*.hpp,*.c,*.h",
+      group = vim.api.nvim_create_augroup("LazyVimCMake", { clear = true }),
       callback = function()
-        local ft = vim.bo.filetype
-        local file_tasks = {
-          cpp = { "Generate CMakeLists", "CMake Configure", "CMake Build + Run" },
-          c = { "Generate CMakeLists", "CMake Configure", "CMake Build + Run" },
-          cs = { "Build & Run C#" },
-          php = { "Run PHP" },
-          javascript = { "Run JavaScript" },
-          html = { "Serve HTML" },
-          python = { "Run Python" },
+        local dir = vim.fn.expand("%:p:h")
+        vim.cmd("silent! cd " .. dir)
+
+        local tasks = {
+          "Generate CMakeLists",
+          "CMake Configure",
+          "CMake Build + Run",
         }
 
-        if file_tasks[ft] then
-          for _, task in ipairs(file_tasks[ft]) do
-            vim.cmd(string.format("OverseerRun '%s'", task))
-          end
+        for _, task in ipairs(tasks) do
+          vim.schedule(function()
+            overseer.run_template({ name = task })
+          end)
         end
       end,
     })
+
+    return opts
   end,
 }
